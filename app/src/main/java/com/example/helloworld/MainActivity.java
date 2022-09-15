572ec41b89fc82b5d508606a094785c7cf3ea66b
Jinpeng Lyu <Louis201307@outlook.com>
2022-09-15 17:55:11 -0400

    Assignment1 Complete

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.helloworld;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Button button;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(view -> text.setText("Hello World."));
+    }
+}
